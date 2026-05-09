2ab72bbfd8af70f4442cd26d1ce14c7bd4e2f7da
Amy Wright <awright@vizabold.com>
2026-05-08 23:09:43 -0400

    added analysis three

--- js/app.js
+++ js/app.js
@@ -198,7 +198,7 @@ comment.oninput = () => {
     }
   }
 
-  ['disclaimer', 'privacy', 'analysis-one', 'analysis-two'].forEach(id => {
+  ['disclaimer', 'privacy', 'analysis-one', 'analysis-two', 'analysis-three'].forEach(id => {
     const el = document.getElementById(id);
     if (el) setupDialog(el);
   });
