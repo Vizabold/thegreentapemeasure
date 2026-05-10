8bbc4dc35f6c531af3c10e91a8e1c4325cb483b8
Amy Wright <awright@vizabold.com>
2026-05-10 17:54:56 -0400

    fixed: file path for ApexCharts import in graphs.js

--- js/graphs.js
+++ js/graphs.js
@@ -1,4 +1,4 @@
-import ApexCharts from "./node_modules/apexcharts/dist/apexcharts.esm.js";
+import ApexCharts from "../node_modules/apexcharts/dist/apexcharts.esm.js";
 
 /* ---------------------- Analysis 1 ------------------------- */
 
