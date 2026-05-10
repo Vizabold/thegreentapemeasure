d8281c81df5c22ec36b68e0802d9dcdb44233efa
Amy Wright <awright@vizabold.com>
2026-05-10 17:52:53 -0400

    fixed: incorrect import path for apexcharts

--- js/graphs.js
+++ js/graphs.js
@@ -1,4 +1,4 @@
-import ApexCharts from "apexcharts";
+import ApexCharts from "./node_modules/apexcharts/dist/apexcharts.esm.js";
 
 /* ---------------------- Analysis 1 ------------------------- */
 
