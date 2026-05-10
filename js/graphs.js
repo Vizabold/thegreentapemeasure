a578934e861ba3de3180f853fc66fa5285371989
Amy Wright <awright@vizabold.com>
2026-05-10 18:21:35 -0400

    fixed: misspelling in graphs.js

--- js/graphs.js
+++ js/graphs.js
@@ -12,7 +12,7 @@ var options = {
                 document.getElementById('pie-chart-1a-placeholder').style.display = 'none';
                 document.getElementById('pie-chart-1a').style.opacity = '1';
             },
-            dataPointSelection: function (event, chartContext, coonfig) {
+            dataPointSelection: function (event, chartContext, config) {
                 if (selectedSliceIndex === config.dataPointIndex) {
                     selectedSliceIndex = '-1';
                 } else {
@@ -31,7 +31,7 @@ var options = {
                 return val.toFixed(1) + '%';
             }
             const icons = ['#bed', '#masks-theater', '#graduation-cap', '#industry', '#house-medical'];
-            return `<svg class="icon-sm fill-current text-neutral-two-dark"><use href="/assets/sprites/solid.svg${icons[opts.seriesIndex]}"></use></svg>`;
+            return `<svg class="icon-sm fill-current text-neutral-two-dark"><use href="../assets/sprites/solid.svg${icons[opts.seriesIndex]}"></use></svg>`;
         },
         style: {
             fontFamily: '"Space Grotesk", sans-serif',
@@ -45,6 +45,9 @@ var options = {
         show: true,
         width: 2,
         color: ['var(--neutral-nine)']
+    },
+    legend: {
+        show: false
     }
 }
 
