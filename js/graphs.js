1766cb696e1e7f4bacd5b669e87b1acf659195a3
Amy Wright <awright@vizabold.com>
2026-05-11 14:59:52 -0400

    added: piechart2a to graphs.js

--- js/graphs.js
+++ js/graphs.js
@@ -18,6 +18,25 @@ const placeholder1b = document.getElementById('pie-chart-1b-placeholder');
 const colors1b = ['var(--primary-one)', 'var(--primary-two)', 'var(--primary-three)', 'var(--primary-four)', 'var(--neutral-seven-light)'];
 let selectedIndex1b = -1;
 
+/* Graph-1c Variables */
+
+/* Graph-1d Variables */
+
+/* Graph-1e Variables */
+
+/* Graph-1f Variables */
+
+/* Graph-1g Variables */
+
+/* Graph-2a Variables */
+const icons2a = ['\u{f1b3}', '\u{f013}', '\u{f1ad}', '\u{e163}', '\u{e3af}', '\u{f508}'];
+const series2a = [25, 22, 19, 17, 9, 8];
+const labels2a = ['fundamentals', 'systems', 'commercial', 'graphics', 'residential', 'practice'];
+const chart2a = document.getElementById('pie-chart-2a');
+const placeholder2a = document.getElementById('pie-chart-2a-placeholder');
+const colors2a = ['var(--primary-one)', 'var(--primary-two)', 'var(--primary-three)', 'var(--primary-four)', 'var(--neutral-seven-light)', 'var(--neutral-eight-light)'];
+let selectedIndex2a = -1;
+
 function piechart(pieIcons, pieSeries, labels, chartEl, placeholder, colors, selectedSliceIndex) {
     function getPieLabelEl(idx) {
         return chartEl.querySelector(`.apexcharts-datalabels[data-realindex="${idx}"] text`)
@@ -129,5 +148,9 @@ function piechart(pieIcons, pieSeries, labels, chartEl, placeholder, colors, sel
 
 piechart(icons1a, series1a, labels1a, chart1a, placeholder1a, colors1a, selectedIndex1a);
 piechart(icons1b, series1b, labels1b, chart1b, placeholder1b, colors1b, selectedIndex1b);
-
-
+/* Evoke function for Graph-1c */
+/* Evoke function for Graph-1d */
+/* Evoke function for Graph-1e */
+/* Evoke function for Graph-1f */
+/* Evoke function for Graph-1g */
+piechart(icons2a, series2a, labels2a, chart2a, placeholder2a, colors2a, selectedIndex1b);
