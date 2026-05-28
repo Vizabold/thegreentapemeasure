fe8bdaa285744a7e2097db3b8165600c092f2843
Amy Wright <awright@vizabold.com>
2026-05-28 15:20:03 -0400

    fix: footer appear logic

--- js/app.js
+++ js/app.js
@@ -11,44 +11,27 @@ const currentYear = new Date().getFullYear();
 year.innerHTML = `${currentYear}`;
 
 /*--------------- FOOTER FADE IN/OUT --------------------- */
-const footer = document.getElementById("main-footer");
-const html = document.documentElement;
-const bottomOffset = 50;
+const footerSentinel = document.getElementById('footer-scroll-sentinel');
+const footer = document.getElementById('main-footer');
 
-const updateFooter = () => {
-  let pageHeight = Math.max(
-    html.clientHeight,
-    html.scrollHeight,
-    html.offsetHeight
-  )
+const footerObserver = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting && footer.classList.contains('opacity-0')) {
+      footer.classList.remove('opacity-0', 'pointer-events-none');
+      footer.classList.add('opacity-100');
+    } else {
+      if (footer.classList.contains('opacity-100')) {
+        footer.classList.remove('opacity-100');
+        footer.classList.add('opacity-0', 'pointer-events-none');
+      }
+    }
+  });
+}, {
+  root: null,
+  threshold: 0.1
+});
 
-  const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-  const viewportHeight = window.innerHeight;
-  const distanceToBottom = pageHeight - (scrollTop + viewportHeight);
-
-  if (distanceToBottom <= bottomOffset) {
-    footer.classList.remove("opacity-0", "pointer-events-none");
-    footer.classList.add("opacity-100");
-  }
-  else {
-    footer.classList.remove("opacity-100");
-    footer.classList.add("opacity-0", "pointer-events-none");
-  }
-}
-
-updateFooter();
-
-window.addEventListener('scroll', updateFooter);
-
-window.addEventListener('resize', () => {
-  pageHeight = Math.max(
-    html.clientHeight,
-    html.scrollHeight,
-    html.offsetHeight
-  );
-  updateFooter();
-})
-
+footerObserver.observe(footerSentinel);
 
 /*--------------- POPOVER LEGACY SUPPORT --------------------- */
 
