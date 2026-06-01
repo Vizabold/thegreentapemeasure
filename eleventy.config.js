ffdda820df15593338ea82f5aa1b65ee3810c3fa
Amy Wright <awright@vizabold.com>
2026-06-01 18:24:20 -0400

    fix: thank you page styling not showing

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,5 @@ export default function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("js");
     eleventyConfig.addPassthroughCopy("assets");
     eleventyConfig.addPassthroughCopy("site.webmanifest");
-    eleventyConfig.addPassthroughCopy("thank-you.html");
 };
 
