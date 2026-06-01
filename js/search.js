14f77ca1df1ba7a08f8a7241a138113b04ca3737
Amy Wright <awright@vizabold.com>
2026-06-01 10:32:55 -0400

    fix: search results now showing

--- js/search.js
+++ js/search.js
@@ -78,8 +78,14 @@ const desktopList = document.getElementById('search-results');
 
 function closeDesktop() {
   desktopList.classList.add('hidden');
+  desktopList.classList.remove('flex');
   desktopInput.setAttribute('aria-expanded', 'false');
 }
+
+desktopForm.addEventListener('submit', e => {
+  e.preventDefault();
+  desktopList.querySelector('[role="option"]')?.click();
+});
 
 desktopInput.addEventListener('input', () => {
   const q = desktopInput.value;
@@ -89,6 +95,7 @@ desktopInput.addEventListener('input', () => {
     desktopInput.value = '';
     closeDesktop();
   });
+  desktopList.classList.remove('hidden');
   desktopList.classList.add('flex');
   desktopInput.setAttribute('aria-expanded', 'true');
 });
