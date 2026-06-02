3f4a53445fe36dc12daa7f958dfaa5c1770bee19
Amy Wright <awright@vizabold.com>
2026-06-02 16:14:57 -0400

    fix: custom cache hashing function not working

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,25 +3,5 @@ export default function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets");
     eleventyConfig.addPassthroughCopy("site.webmanifest");
     eleventyConfig.addPassthroughCopy("_headers");
-
-    const buildHash = Date.now().toString(36);
-
-    eleventyConfig.addTransform("cacheBuster", function (content) {
-        const outputPath = this.page.outputPath;
-
-        if (outputPath && outputPath.endsWith(".html")) {
-            return content
-                .replace(/(href="\/css\/[^"|?]+)/g, `$1?v=${buildHash}`)
-                .replace(/(src="\/js\/[^"|?]+)/g, `$1?v=${buildHash}`);
-        }
-        return content;
-    });
-
-    return {
-        dir: {
-            input: ".",
-            output: "_site"
-        }
-    };
 };
 
