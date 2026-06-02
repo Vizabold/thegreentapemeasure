508e0533171430cea3c9deb7dd0f3c0f66d32951
Amy Wright <awright@vizabold.com>
2026-06-02 14:29:18 -0400

    fix: eleventy config error

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,4 @@
-import { default as EleventyAssetHash } from "@vrugtehagel/eleventy-asset-hash";
+import fs from "fs";
 
 export default function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("js");
@@ -6,10 +6,17 @@ export default function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("site.webmanifest");
     eleventyConfig.addPassthroughCopy("_headers");
 
-    eleventyConfig.addPlugin(EleventyAssetHash, {
-        algorithm: "SHA-256",
-        include: ["**/*.html"],
-        includeAssets: ["**/*.{css,js}"]
+    const buildHash = Date.now().toString(36);
+
+    eleventyConfig.addTransform("cacheBuster", function (content, outputPath) {
+        if (outputPath && outputPath.endsWith(".html")) {
+            let updatedContent = content
+                .replace(/(href="\/css\/[^"|?]+)/g, `$1?v=${buildHash}`)
+                .replace(/(src="\/js\/[^"|?]+)/g, `$1?v=${buildHash}`);
+
+            return updatedContent;
+        }
+        return content;
     });
 
     return {
