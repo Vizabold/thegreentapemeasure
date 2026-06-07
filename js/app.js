934268926e39ba1aedb4cadc4f39ea1d7c721589
Amy Wright <awright@vizabold.com>
2026-06-07 09:07:41 -0400

    fix: accordions not animating on firefox or apple devices

--- js/app.js
+++ js/app.js
@@ -300,3 +300,24 @@ if (videoWrapper) {
     iframe.focus();
   });
 }
+
+/*------------------------------ DETAILS TRANSITION FALLBACK --------------------------------- */
+document.querySelectorAll('details').forEach((details) => {
+  details.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const content = details.querySelector('.accordion-content-wrapper');
+
+    if (details.open) {
+      details.classList.remove('is-open');
+      setTimeout(() => {
+        details.removeAttribute('open');
+      }, 500);
+    } else {
+      details.setAttribute('open', 'true');
+      requestAnimationFrame(() => {
+        details.classList.add('is-open');
+      });
+    }
+  });
+});
